92d4ce565c221ebc07067afab4622a98c2c0f731
cgates <cgates@bfx-comp3.ccmb.med.umich.edu>
2013-06-14 15:28:07 -0400

    cgates/pulintz: extending SplitRead

--- bin/SplitRead.py
+++ bin/SplitRead.py
@@ -8,17 +8,61 @@ import resource
 class FQStanza(object):
 	""" Encapsulates the four line chunks of a fastq file. """
 	
-	def __init__(self, line_str, delim="\n"):
-		(self.main_header, self.seq, self.score_header, self.score) = \
-			line_str.rstrip().split(delim)
+	@classmethod
+	def parse_input(cls, line_str, delim="\n"):
+		return FQStanza(*line_str.rstrip().split(delim))
 		
-		
+	def __init__(self, main_header, seq, score_header, score):	
+		(self.main_header, self.seq, self.score_header, self.score) = (main_header, seq, score_header, score)
 
 
+	def split(self, split_position):
+		right_size = len(self.seq)-split_position
+		(left_seq, right_seq) = (self.seq[0:split_position], self.seq[split_position:])
+		(left_score, right_score) = (self.score[0:split_position], self.score[split_position:])
+		left_main_header = "{0}-L-{1}".format(self.main_header, split_position)
+		left_score_header = "{0}-L-{1}".format(self.score_header, split_position)
+		right_main_header = "{0}-R-{1}".format(self.main_header, right_size)
+		right_score_header = "{0}-R-{1}".format(self.score_header, right_size)
 
-	
-	
-	
+		left_stanza = FQStanza(left_main_header, left_seq, left_score_header, left_score)
+		right_stanza = FQStanza(right_main_header, right_seq, right_score_header, right_score)
+
+		return (left_stanza, right_stanza)
+
+	def __repr__(self):
+		return "{0}\n{1}\n{2}\n{3}".format(self.main_header, self.seq, self.score_header, self.score)
+
+def build_splits(in_stanza, split_margin):
+	stanzas = []
+	for split_position in range(split_margin, (len(in_stanza.seq)-split_margin)+1):
+		stanzas.extend(in_stanza.split(split_position))
+	return stanzas
+
+
+def write_stanzas(in_stanzas, writer, split_margin):
+	for in_stanza in in_stanzas:
+		for out_stanza in build_splits(in_stanza, split_margin):
+			writer.write(str(out_stanza))
+			writer.write("\n")
+
+
+def stanza_generator(reader, stanza_delimiter)
+	for line in reader:
+		stanza_str = line
+		if stanza_str.startswith(stanza_delimiter):
+			break
+
+	for line in reader:
+		if line.startswith(stanza_delimiter):
+			yield stanza_str
+			stanza_str = line
+		stanza_str += line
+
+# def process_file():
+# 	generator = stanza_generator(reader)
+# 	write_stanzas(generator, kjdhsfk)
+
 if __name__ == "__main__":
 
 	if (len(sys.argv)  < 4 or len(sys.argv) > 5):
--- test/SplitReadTest.py
+++ test/SplitReadTest.py
@@ -8,22 +8,112 @@ import unittest
 import tempfile
 import os
 
-from SplitRead import FQStanza
+from SplitRead import FQStanza, build_splits, write_stanzas, stanza_generator
 
 
 class FQStanzaTest(unittest.TestCase):
 	
-	def test_init(self):
+	def test_parse_input(self):
 		stanza_string = "@HWI-EAS159:6:1:6:610#0/1\nGCACGGTTCTGTAGTCTNCAGAAGTATCNGATNNG\n+HWI-EAS159:6:1:6:610#0/1\naaa^W\]a]^Z^]P[_[DZWR[\\\BBBBBBBBBB\n"
-		actual_stanza = FQStanza(stanza_string)
+		actual_stanza = FQStanza.parse_input(stanza_string)
 		
 		self.assertEqual("@HWI-EAS159:6:1:6:610#0/1", actual_stanza.main_header)
 		self.assertEqual("GCACGGTTCTGTAGTCTNCAGAAGTATCNGATNNG", actual_stanza.seq)
 		self.assertEqual("+HWI-EAS159:6:1:6:610#0/1", actual_stanza.score_header)
 		self.assertEqual("aaa^W\]a]^Z^]P[_[DZWR[\\\BBBBBBBBBB", actual_stanza.score)
+	
+	def test_split(self):
+		inputHeader = "header"
+		seq = "ABCDE"
+		score_header = "score_header"
+		score="12345"
+		
+		inputStanza = FQStanza(inputHeader, seq, score_header, score)
+		split_position = 2
+		
+		(actualLeftStanza, actualRightStanza) = inputStanza.split(split_position)
+
+		self.assertEquals("header-L-2", actualLeftStanza.main_header)
+		self.assertEquals("AB", actualLeftStanza.seq)
+		self.assertEquals("score_header-L-2", actualLeftStanza.score_header)
+		self.assertEquals("12", actualLeftStanza.score)
+		
+		self.assertEquals("header-R-3", actualRightStanza.main_header)
+		self.assertEquals("CDE", actualRightStanza.seq)
+		self.assertEquals("score_header-R-3", actualRightStanza.score_header)
+		self.assertEquals("345", actualRightStanza.score)
+		
+	def test_repr(self):
+		input_header = "header"
+		seq = "ABCDE"
+		score_header = "score_header"
+		score="12345"
+		
+		input_stanza = FQStanza(input_header, seq, score_header, score)
+		
+		self.assertEquals("{0}\n{1}\n{2}\n{3}".format(input_header, seq, score_header, score), str(input_stanza))
+		
+class SplitReadTest(unittest.TestCase):
+		
+	def test_build_splits(self):
+		inputHeader = "header"
+		seq = "ABCDE"
+		score_header = "score_header"
+		score="12345"
+		
+		inputStanza = FQStanza(inputHeader, seq, score_header, score)
+		split_margin = 2
+		
+		stanza_list = build_splits(inputStanza, split_margin)
+		
+		self.assertEquals(4, len(stanza_list))
+		self.assertEquals("AB", stanza_list[0].seq)
+		self.assertEquals("CDE", stanza_list[1].seq)
+		self.assertEquals("ABC", stanza_list[2].seq)
+		self.assertEquals("DE", stanza_list[3].seq)
+		
+	def test_write_stanzas(self):
+		stanzaA = init_stanza("headerA")
+		stanzaB = init_stanza("headerB")
+		writer = MockWriter()
+		split_margin = 2
+
+		write_stanzas([stanzaA, stanzaB], writer, split_margin)		
+
+		lines = writer.lines()
+		self.assertEquals(8 * 4, len(lines))
+		stanzaA_headers = [s for s in lines if s.startswith("headerA")]
+		self.assertEquals(4, len(stanzaA_headers))
+
+	def test_stanza_generator(self):
+		reader = ["file_header1","file_header2","@h1","ABC","h1","123","@h2","DEF","+h2","456"]
+		stanza_length = 4
+		
+		stanzas = [thing for thing in stanza_generator(reader, "@", stanza_length)]
+
+		self.assertEquals(2, len(stanzas))
+		self.assertEquals("@h1", stanzas[0].main_header)
+		self.assertEquals("@h2", stanzas[1].main_header)
 
 
+def init_stanza(header):
+	return FQStanza(header, "ABCDE", "score_header", "score")
+		
+		
+class MockWriter():
+	def __init__(self):
+		self._content = []
+		self.wasClosed = False
 
+	def write(self, content):
+		self._content.append(content)
+	
+	def lines(self):
+		return "".join(self._content).splitlines()
+
+	def close(self):
+		self.wasClosed = True
+	
 if __name__ == "__main__":
 	unittest.main() 
 	print "done."
