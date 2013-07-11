288a9998c1a2bc59b581abbf9553d57a34fb5206
cgates <cgates@bfx-comp3.ccmb.med.umich.edu>
2013-07-11 12:12:45 -0400

    cgates: renamed bed_foo to gap_foo and adjusted format of gap_file output to include gap width and read width

--- bin/cluster_gaps.py
+++ bin/cluster_gaps.py
@@ -31,13 +31,13 @@ class StdErrLogger():
                 else:
                         self._log(message)
 
-class BedUtility():
+class GapUtility():
 	def __init__(self, original_read_len, delimiter):
 		self._original_read_len = int(original_read_len)
 		self._delimiter = delimiter
 		self._name_re = re.compile(r"(.+)-([LR])-([\d]+)$")
 
-	def bed_line(self, sam_line):
+	def gap_line(self, sam_line):
 		(split_read_name, flag, transcript_name, start_pos, mapq_score, cigar, rnext, pnext, tlen, seq) = sam_line.split(self._delimiter)[0:10]
 		l_gap_pos = int(start_pos) + len(seq)
 		r_gap_pos = int(pnext)
@@ -46,64 +46,64 @@ class BedUtility():
 		strand = "+" if tlen > 0 else "-"
 		m = self._name_re.match(split_read_name)
                 (original_read_name, side, split_len) = (m.group(1), m.group(2), m.group(3))
-		return self._delimiter.join([transcript_name, str(l_gap_pos), str(r_gap_pos), split_read_name, mapq_score, strand, str(leftmost_start), str(rightmost_end), original_read_name])
+		return self._delimiter.join([transcript_name, str(l_gap_pos), str(r_gap_pos), str(r_gap_pos - l_gap_pos), str(leftmost_start), str(rightmost_end), str(rightmost_end - leftmost_start), split_read_name, original_read_name])
 
-	def samfile_to_bed(self, sam_file):
-		bed_lines=["#chrom|gapStart|gapEnd|readName|score|strand|readStart|readEnd"]
+	def samfile_to_gap_lines(self, sam_file):
+		gap_lines=["#chromosome|gapStart|gapEnd|gapWidth|readStart|readEnd|readWidth|splitReadName|originalReadName"]
 		for line in sam_file:
 			if line.startswith("@"):
 				continue
 			tlen = int(line.split(self._delimiter)[8])
 			if tlen > 0: 
-				bed_lines.append(self.bed_line(line))
-		return bed_lines
+				gap_lines.append(self.gap_line(line))
+		return gap_lines
 
-	def sort_bed_lines(self, bed_lines):
+	def sort_gap_lines(self, gap_lines):
 		def line_key(line):
 			if line.startswith("#"):
 				return ("", 0) 
 			(chrom, start) = line.split(self._delimiter)[0:2]
 			return (chrom, int(start))
-		return sorted(bed_lines, key=line_key)
+		return sorted(gap_lines, key=line_key)
 
-	def write_bed_file(self, bed_lines, writer, additional_header_lines=[]):
+	def write_gap_file(self, gap_lines, writer, additional_header_lines=[]):
 		for line in additional_header_lines:
 			writer.write("#")
 			writer.write(line)
 			writer.write("\n")
-		for line in bed_lines:
+		for line in gap_lines:
 			writer.write(line)
 			writer.write("\n")
 
-def main(sam_file_name, original_read_len, bed_file_name, delimiter):
+def main(sam_file_name, original_read_len, gap_file_name, delimiter):
  	logger = StdErrLogger(verbose=True)
 	logger.log(" ".join(sys.argv), verbose=False)
 	header_lines = [str(datetime.datetime.today()), " ".join(sys.argv)] 
-	bed_utility = BedUtility(original_read_len, delimiter)
+	gap_utility = BedUtility(original_read_len, delimiter)
 	
 	logger.log("parsing sam file")
 	sam_file = open(sam_file_name,"r")
-	bed_lines = bed_utility.samfile_to_bed(sam_file)
+	gap_lines = gap_utility.samfile_to_gap_lines(sam_file)
 	sam_file.close()
 
-	logger.log("sorting bed lines")	
-	bed_lines = bed_utility.sort_bed_lines(bed_lines)
+	logger.log("sorting gap lines")	
+	gap_lines = gap_utility.sort_gap_lines(gap_lines)
 
-	logger.log("writing bed file")
-	bed_file = open(bed_file_name, "w")
-	bed_utility.write_bed_file(bed_lines, bed_file, header_lines)
-	bed_file.close()
+	logger.log("writing gap file")
+	gap_file = open(gap_file_name, "w")
+	gap_utility.write_gap_file(gap_lines, gap_file, header_lines)
+	gap_file.close()
 	
 	logger.log("complete")
 	
 if __name__ == "__main__":
 	if (len(sys.argv) != 4):
-		print "usage: {0} [sam_file] [original_read_len] [bed_file]".format(os.path.basename(sys.argv[0]))
+		print "usage: {0} [sam_file] [original_read_len] [gap_file]".format(os.path.basename(sys.argv[0]))
 		sys.exit() 
 
-	(sam_file_name, original_read_len, bed_file_name) = sys.argv[1:]
+	(sam_file_name, original_read_len, gap_file_name) = sys.argv[1:]
 	sam_file_name = os.path.abspath(sam_file_name)
-	bed_file_name = os.path.abspath(bed_file_name)
+	gap_file_name = os.path.abspath(gap_file_name)
 
-	main(sam_file_name, original_read_len, bed_file_name, "\t") 
+	main(sam_file_name, original_read_len, gap_file_name, "\t") 
 	print "done."
--- test/cluster_gaps_test.py
+++ test/cluster_gaps_test.py
@@ -2,31 +2,31 @@
 
 import sys ; sys.path.insert(0, "../bin")
 import unittest
-from cluster_gaps import BedUtility
+from cluster_gaps import GapUtility
 
 
 class ClusterGapsTest(unittest.TestCase):
 
-	def test_bed_line(self):	
+	def test_gap_line(self):	
 		original_read_len = 50
 		delimiter = "|"
 		sam_line = "read-name-L-42|67|transcript42|100|score|cigar|=|150|50|ACGCT|CCCFF|XA:foo"
 
-		actual_bed_line = BedUtility(original_read_len, delimiter).bed_line(sam_line)
+		actual_bed_line = GapUtility(original_read_len, delimiter).gap_line(sam_line)
 		
 		actual_bits = actual_bed_line.split(delimiter)
 		self.assertEqual(9, len(actual_bits), actual_bed_line)
 		self.assertEqual("transcript42", actual_bits[0])
 		self.assertEqual("105", actual_bits[1])
 		self.assertEqual("150", actual_bits[2])
-		self.assertEqual("read-name-L-42", actual_bits[3])
-		self.assertEqual("score", actual_bits[4])
-		self.assertEqual("+", actual_bits[5])
-		self.assertEqual("100", actual_bits[6])
-		self.assertEqual("195", actual_bits[7])
+		self.assertEqual("45", actual_bits[3])
+		self.assertEqual("100", actual_bits[4])
+		self.assertEqual("195", actual_bits[5])
+		self.assertEqual("95", actual_bits[6])
+		self.assertEqual("read-name-L-42", actual_bits[7])
 		self.assertEqual("read-name", actual_bits[8])
 
-	def test_samfile_to_bedfile_skipsNegativeStrand(self):		
+	def test_samfile_to_gap_lines_skipsNegativeStrand(self):		
 		original_read_len = 50
 		delimiter="|"
 		sam_file = ["@header1", "@header2", 
@@ -35,45 +35,43 @@ class ClusterGapsTest(unittest.TestCase):
 				"read2-L-1|113|transcript43|205|score|cigar|=|155|-50|ACGCT|qual|stuff",
 				"read2-L-1|67|transcript43|155|score|cigar|=|205|50|ACGCT|qual|stuff"]
 
-		writer = MockWriter()
+		gap_lines = GapUtility(original_read_len, delimiter).samfile_to_gap_lines(sam_file)
 
-		bed_lines = BedUtility(original_read_len, delimiter).samfile_to_bed(sam_file)
-
-		actual_lines = sorted(bed_lines)
+		actual_lines = sorted(gap_lines)
 		self.assertEqual(3, len(actual_lines))
-		self.assertEqual("#chrom|gapStart|gapEnd|readName|score|strand|readStart|readEnd", actual_lines[0])
+		self.assertEqual("#chromosome|gapStart|gapEnd|gapWidth|readStart|readEnd|readWidth|splitReadName|originalReadName", actual_lines[0])
 		self.assertEqual(True, actual_lines[1].startswith("transcript42"))
 		self.assertEqual(True, actual_lines[2].startswith("transcript43"))
 
-	def test_sort_bed_lines_sortedByChromByAlphaAndGapStartByNumeric(self):
+	def test_sort_gap_lines_sortedByChromByAlphaAndGapStartByNumeric(self):
 		original_read_len = 50
 		delimiter = "|"
 		sorted_lines = ["chrom1|1|foo",
-                                "chrom10|1|bar",
-                                "chrom2|1|uno",
+				"chrom10|1|bar",
+				"chrom2|1|uno",
 				"chrom2|2|dos",
 				"chrom2|10|diez",
 				"chrom2|11|once",
-                                "chrom20|1|froody"]
+				"chrom20|1|froody"]
 
 		input_lines = ["#header42"] 
 		input_lines.extend(sorted_lines[::-1])
 		input_lines.extend(["#header1"]) 
 
-                actual_lines = BedUtility(original_read_len, delimiter).sort_bed_lines(input_lines)
+		actual_lines = GapUtility(original_read_len, delimiter).sort_gap_lines(input_lines)
 
-                self.assertEqual(len(input_lines), len(actual_lines))
+		self.assertEqual(len(input_lines), len(actual_lines))
 		self.assertEqual(["#header42","#header1"], actual_lines[:2])
 		self.assertEqual(sorted_lines, actual_lines[2:])
 
-	def test_write_bed_file(self):
+	def test_write_gap_file(self):
 		original_read_len = 50
-                delimiter = "|"
+		delimiter = "|"
 		additional_header_lines=["hoopy", "frood"]
-                input_lines = ["#foo", "bar","baz"]
-                writer = MockWriter()
+		input_lines = ["#foo", "bar","baz"]
+		writer = MockWriter()
 
-		BedUtility(original_read_len, delimiter).write_bed_file(input_lines, writer, additional_header_lines)
+		GapUtility(original_read_len, delimiter).write_gap_file(input_lines, writer, additional_header_lines)
 
 		actual_lines = writer.lines()
 		expected_lines = ["#hoopy", "#frood"]
