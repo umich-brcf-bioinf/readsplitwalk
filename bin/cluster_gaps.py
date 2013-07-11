9c484b6a8db51da84bab5ddd7f9960bcf35c0f0a
cgates <cgates@bfx-comp3.ccmb.med.umich.edu>
2013-07-11 11:48:42 -0400

    cgates: refactored to emit sorted bed files

--- bin/cluster_gaps.py
+++ bin/cluster_gaps.py
@@ -5,44 +5,105 @@ cluster_gaps.py
 7/10/2013 - cgates/pulintz
 """
 
-import sys
+import datetime
 import os
+import re
+import resource
+import sys
+import traceback
 
-def bed_line(sam_line, original_read_len, delimiter="\t", name_delimiter="|"):
-	(rname, flag, transcript_name, start_pos, mapq, cigar, rnext, pnext, tlen, seq) = sam_line.split(delimiter)[0:10]
-	l_gap_pos = int(start_pos) + len(seq)
-	r_gap_pos = int(pnext) - 1
-	leftmost_start = int(start_pos)
-	rightmost_end = int(pnext) + (original_read_len - len(seq))
-	name = name_delimiter.join([rname, str(leftmost_start), str(rightmost_end)])  
-	return delimiter.join([transcript_name, str(l_gap_pos), str(r_gap_pos), name]) + "\n"
+class StdErrLogger():
+        """Writes basic utilization data to stderr"""
+        def __init__(self, verbose = False):
+                self._verbose = verbose
+
+        def _log(self, message):
+                print >> sys.stderr, "{0}|{1}".format(datetime.datetime.today(), message)
 
 
-def samfile_to_bedfile(sam_file, original_read_len, writer, field_delimiter="\t", name_delimiter="|"):
-	for line in sam_file:
-		if line.startswith("@"):
-			continue
-		tlen = int(line.split(field_delimiter)[8])
-		if tlen > 0: 
-			writer.write(bed_line(line, original_read_len, field_delimiter, name_delimiter))
+        def log(self, message, verbose=None):
+		verbose = self._verbose if verbose is None else verbose 
+                if (verbose):
+                        usage = resource.getrusage(resource.RUSAGE_SELF)
+                        memory_used = usage.ru_maxrss/1024
+                        function_name = traceback.extract_stack()[-2:-1][0][2]
+                        self._log("usertime(s)={0:.0f}|systime(s)={1:.0f}|peak_memory_used(mb)={2}|{3}|{4}".format(usage.ru_utime, usage.ru_stime, memory_used, function_name, message))
+                else:
+                        self._log(message)
 
-def main(sam_file_name, bed_file_name, delimiter="\t"):
-	sam_file = open(sam_file_name,"r")
-	bed_file = open(bed_file_name, "w")
+class BedUtility():
+	def __init__(self, original_read_len, delimiter):
+		self._original_read_len = int(original_read_len)
+		self._delimiter = delimiter
+		self._name_re = re.compile(r"(.+)-([LR])-([\d]+)$")
+
+	def bed_line(self, sam_line):
+		(split_read_name, flag, transcript_name, start_pos, mapq_score, cigar, rnext, pnext, tlen, seq) = sam_line.split(self._delimiter)[0:10]
+		l_gap_pos = int(start_pos) + len(seq)
+		r_gap_pos = int(pnext)
+		leftmost_start = int(start_pos)
+		rightmost_end = int(pnext) + (self._original_read_len - len(seq))
+		strand = "+" if tlen > 0 else "-"
+		m = self._name_re.match(split_read_name)
+                (original_read_name, side, split_len) = (m.group(1), m.group(2), m.group(3))
+		return self._delimiter.join([transcript_name, str(l_gap_pos), str(r_gap_pos), split_read_name, mapq_score, strand, str(leftmost_start), str(rightmost_end), original_read_name])
+
+	def samfile_to_bed(self, sam_file):
+		bed_lines=["#chrom|gapStart|gapEnd|readName|score|strand|readStart|readEnd"]
+		for line in sam_file:
+			if line.startswith("@"):
+				continue
+			tlen = int(line.split(self._delimiter)[8])
+			if tlen > 0: 
+				bed_lines.append(self.bed_line(line))
+		return bed_lines
+
+	def sort_bed_lines(self, bed_lines):
+		def line_key(line):
+			if line.startswith("#"):
+				return ("", 0) 
+			(chrom, start) = line.split(self._delimiter)[0:2]
+			return (chrom, int(start))
+		return sorted(bed_lines, key=line_key)
+
+	def write_bed_file(self, bed_lines, writer, additional_header_lines=[]):
+		for line in additional_header_lines:
+			writer.write("#")
+			writer.write(line)
+			writer.write("\n")
+		for line in bed_lines:
+			writer.write(line)
+			writer.write("\n")
+
+def main(sam_file_name, original_read_len, bed_file_name, delimiter):
+ 	logger = StdErrLogger(verbose=True)
+	logger.log(" ".join(sys.argv), verbose=False)
+	header_lines = [str(datetime.datetime.today()), " ".join(sys.argv)] 
+	bed_utility = BedUtility(original_read_len, delimiter)
 	
-	samfile_to_bedfile(sam_file, bed_file, delimiter)
-
+	logger.log("parsing sam file")
+	sam_file = open(sam_file_name,"r")
+	bed_lines = bed_utility.samfile_to_bed(sam_file)
 	sam_file.close()
+
+	logger.log("sorting bed lines")	
+	bed_lines = bed_utility.sort_bed_lines(bed_lines)
+
+	logger.log("writing bed file")
+	bed_file = open(bed_file_name, "w")
+	bed_utility.write_bed_file(bed_lines, bed_file, header_lines)
 	bed_file.close()
 	
+	logger.log("complete")
+	
 if __name__ == "__main__":
-	if (len(sys.argv) != 3):
-		print "usage: {0} [sam_file] [bed_file]".format(os.path.basename(sys.argv[0]))
+	if (len(sys.argv) != 4):
+		print "usage: {0} [sam_file] [original_read_len] [bed_file]".format(os.path.basename(sys.argv[0]))
 		sys.exit() 
 
-	(sam_file_name, bed_file_name) = sys.argv[1:]
+	(sam_file_name, original_read_len, bed_file_name) = sys.argv[1:]
 	sam_file_name = os.path.abspath(sam_file_name)
 	bed_file_name = os.path.abspath(bed_file_name)
 
-	main(sam_file_name, bed_file_name, "\t", "|") 
+	main(sam_file_name, original_read_len, bed_file_name, "\t") 
 	print "done."
--- test/cluster_gaps_test.py
+++ test/cluster_gaps_test.py
@@ -2,36 +2,83 @@
 
 import sys ; sys.path.insert(0, "../bin")
 import unittest
-from cluster_gaps import bed_line, samfile_to_bedfile
+from cluster_gaps import BedUtility
 
 
 class ClusterGapsTest(unittest.TestCase):
 
-	def test_bed_line(self):
-		
+	def test_bed_line(self):	
 		original_read_len = 50
-		sam_line = "read_1-stuff-L-41|67|transcript42|100|score|cigar|=|150|50|ACGCT|CCCFF|XA:foo"
-		field_delimiter = "|"
-		name_delimiter = "#"
-		actual_bed_line = bed_line(sam_line, original_read_len, field_delimiter, name_delimiter)
-		self.assertEqual("transcript42|105|149|read_1-stuff-L-41#100#195\n", actual_bed_line)
+		delimiter = "|"
+		sam_line = "read-name-L-42|67|transcript42|100|score|cigar|=|150|50|ACGCT|CCCFF|XA:foo"
 
-	def test_samfile_to_bedfile(self):		
+		actual_bed_line = BedUtility(original_read_len, delimiter).bed_line(sam_line)
+		
+		actual_bits = actual_bed_line.split(delimiter)
+		self.assertEqual(9, len(actual_bits), actual_bed_line)
+		self.assertEqual("transcript42", actual_bits[0])
+		self.assertEqual("105", actual_bits[1])
+		self.assertEqual("150", actual_bits[2])
+		self.assertEqual("read-name-L-42", actual_bits[3])
+		self.assertEqual("score", actual_bits[4])
+		self.assertEqual("+", actual_bits[5])
+		self.assertEqual("100", actual_bits[6])
+		self.assertEqual("195", actual_bits[7])
+		self.assertEqual("read-name", actual_bits[8])
+
+	def test_samfile_to_bedfile_skipsNegativeStrand(self):		
 		original_read_len = 50
+		delimiter="|"
 		sam_file = ["@header1", "@header2", 
-				"read1|67|transcript42|150|score|cigar|=|200|50|ACGCT|qual|stuff", 
-				"read1|113|transcript42|200|score|cigar|=|150|-50|ACGCT|qual|stuff",
-				"read2|113|transcript43|205|score|cigar|=|155|-50|ACGCT|qual|stuff",
-				"read2|67|transcript43|155|score|cigar|=|205|50|ACGCT|qual|stuff"]
+				"read1-L-1|67|transcript42|150|score|cigar|=|200|50|ACGCT|qual|stuff", 
+				"read1-L-1|113|transcript42|200|score|cigar|=|150|-50|ACGCT|qual|stuff",
+				"read2-L-1|113|transcript43|205|score|cigar|=|155|-50|ACGCT|qual|stuff",
+				"read2-L-1|67|transcript43|155|score|cigar|=|205|50|ACGCT|qual|stuff"]
 
 		writer = MockWriter()
 
-		samfile_to_bedfile(sam_file, original_read_len, writer, "|")
+		bed_lines = BedUtility(original_read_len, delimiter).samfile_to_bed(sam_file)
 
-		actual_lines = sorted(writer.lines())
-		self.assertEqual(2, len(actual_lines))
-		self.assertEqual(True, actual_lines[0].startswith("transcript42"))
-		self.assertEqual(True, actual_lines[1].startswith("transcript43"))
+		actual_lines = sorted(bed_lines)
+		self.assertEqual(3, len(actual_lines))
+		self.assertEqual("#chrom|gapStart|gapEnd|readName|score|strand|readStart|readEnd", actual_lines[0])
+		self.assertEqual(True, actual_lines[1].startswith("transcript42"))
+		self.assertEqual(True, actual_lines[2].startswith("transcript43"))
+
+	def test_sort_bed_lines_sortedByChromByAlphaAndGapStartByNumeric(self):
+		original_read_len = 50
+		delimiter = "|"
+		sorted_lines = ["chrom1|1|foo",
+                                "chrom10|1|bar",
+                                "chrom2|1|uno",
+				"chrom2|2|dos",
+				"chrom2|10|diez",
+				"chrom2|11|once",
+                                "chrom20|1|froody"]
+
+		input_lines = ["#header42"] 
+		input_lines.extend(sorted_lines[::-1])
+		input_lines.extend(["#header1"]) 
+
+                actual_lines = BedUtility(original_read_len, delimiter).sort_bed_lines(input_lines)
+
+                self.assertEqual(len(input_lines), len(actual_lines))
+		self.assertEqual(["#header42","#header1"], actual_lines[:2])
+		self.assertEqual(sorted_lines, actual_lines[2:])
+
+	def test_write_bed_file(self):
+		original_read_len = 50
+                delimiter = "|"
+		additional_header_lines=["hoopy", "frood"]
+                input_lines = ["#foo", "bar","baz"]
+                writer = MockWriter()
+
+		BedUtility(original_read_len, delimiter).write_bed_file(input_lines, writer, additional_header_lines)
+
+		actual_lines = writer.lines()
+		expected_lines = ["#hoopy", "#frood"]
+		expected_lines.extend(input_lines)
+		self.assertEqual(expected_lines, actual_lines)
 
 
 class MockWriter():
